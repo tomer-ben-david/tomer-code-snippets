2b185969bfe57cfc45b2b7a6857ee713d42c5cc0
tomer-ben-david <tomerbd1@gmail.com>
2018-07-24 22:50:09 +0300

    added sentence splitter stanford nlp example

---
+++ java/java-snippets/src/main/java/org/tomerbd/snippets/nlp/BasicNLP.java
@@ -0,0 +1,16 @@
+package org.tomerbd.snippets.nlp;
+
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BasicNLP {
+    public static void main(String[] args) throws Exception {
+
+
+    }
+}
---
+++ java/java-snippets/src/main/java/org/tomerbd/snippets/nlp/StandfordNLP.java
@@ -0,0 +1,35 @@
+package org.tomerbd.snippets.nlp;
+
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.List;
+
+public class StandfordNLP {
+    public static void main(String[] args) throws Exception {
+        StandfordNLP standfordNLP = new StandfordNLP();
+        standfordNLP.sentenceSplitter();
+    }
+
+    public void sentenceSplitter() {
+        String paragraph = "The first sentence. The second sentence.";
+        Reader reader = new StringReader(paragraph);
+        DocumentPreprocessor documentPreprocessor = new DocumentPreprocessor(reader);
+        List<String> sentenceList = new LinkedList<String>();
+        for (List<HasWord> element: documentPreprocessor) {
+            StringBuilder sentence = new StringBuilder();
+            List<HasWord> hasWordList = element;
+            for (HasWord token : hasWordList) {
+                sentence.append(token).append(" ");
+            }
+            sentenceList.add(sentence.toString());
+        }
+
+        for (String sentence : sentenceList) {
+            System.out.println(sentence);
+        }
+    }
+}
